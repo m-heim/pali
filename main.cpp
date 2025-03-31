489ccef5367f824b5afd4bd854afcad47e005811
m-heim <maximilian.heim@protonmail.com>
2025-03-31 20:48:32 +0200

    Update example

--- main.cpp
+++ main.cpp
@@ -13,18 +13,21 @@ int main() {
     PixelProperties pp = PixelProperties(value, RGB(74, 74, 74));
     std::random_device rd;  // a seed source for the random number engine
     std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(-4, 4);
+    std::uniform_int_distribution<> distrib(-10, 10);
     
     int x = distrib(gen);
     if (x == 0) {
         x = 1;
     }
+    int r = distrib(gen);
+    r += 30;
+    r /= 10;
     int y = distrib(gen);
     if (y == 0) {
         y = 1;
     }
-    auto po = std::make_unique<PixelObject>(PixelObject(Point(42, 42), pp));
-    po->setVelocity(Point(x,y));
+    auto po = std::make_unique<SphereObject>(SphereObject(Point(42, 42), pp, r));
+    po->setVelocity(Point(x / 24.0,y / 24.0));
 
     engine.addObject(std::move(po));
         std::cout << "Looping\n";
