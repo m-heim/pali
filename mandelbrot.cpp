d2a731a475e58cd9699b27cd59cd785dc3a29b24
m-heim <maximilian.heim@protonmail.com>
2025-04-08 23:18:31 +0200

    Update examples

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,8 +1,8 @@
 #include "src/pali.hpp"
-#include <memory>
-#include <unistd.h>
-#include <string>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <unistd.h>
 
 #define HEIGHT 84
 #define WIDTH 242
@@ -25,7 +25,7 @@ int main(int argc, char **argv) {
   std::vector<uint64_t> v;
   while (1) {
     PixelProperties pp = PixelProperties(value, RGB(0, 0, 0), RGB(0, 0, 0));
-    for (int i = 0; i < HEIGHT; i++) {   // y axis
+    for (int i = 0; i < HEIGHT; i++) {  // y axis
       for (int j = 0; j < WIDTH; j++) { // x axis
         double x = px + (((j - (WIDTH / 2.0)) / (WIDTH / 2.0)) * vxy);
         double y = py + (((i - (HEIGHT / 2.0)) / (HEIGHT / 2.0)) * vxy);
@@ -36,16 +36,15 @@ int main(int argc, char **argv) {
           zj = zj * zj - zi * zi + x;
           zi = zj2 * zi + zj2 * zi + y;
           if (zj * zj + zi * zi > 4) {
-            //std::cout << "Diverged" << std::to_string(v) << std::endl;
-            pp.setColor2(
-                RGB(v, (128 - v) % 256, 255 - v));
+            // std::cout << "Diverged" << std::to_string(v) << std::endl;
+            pp.setColor2(RGB(v, (128 - v) % 256, 255 - v));
             break;
           }
           if (v == 255) {
             pp.setColor2(RGB(0, 0, 0));
           }
         }
-        //std::cout << std::to_string(zi * zi + zj * zj) << std::endl;
+        // std::cout << std::to_string(zi * zi + zj * zj) << std::endl;
         auto po = std::make_unique<PixelObject>(PixelObject(Point(j, i), pp));
         uint64_t ve = engine.addObject(std::move(po));
         v.push_back(ve);
@@ -53,12 +52,12 @@ int main(int argc, char **argv) {
     }
     // po->setVelocity(Point(x / 24.0,y / 24.0));
 
-    //std::cout << "Looping\n";
+    // std::cout << "Looping\n";
     engine.loop();
-    //std::cout << 1 / vxy << std::endl;
+    // std::cout << 1 / vxy << std::endl;
     usleep(100000);
     engine.emptyObjs();
-    
+
     vxy = vxy / 1.031;
   }
   return 0;
