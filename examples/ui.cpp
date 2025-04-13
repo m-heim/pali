7ed8ec0ef8c6eeab763956f1dc8b69f1560b6ae1
m-heim <maximilian.heim@protonmail.com>
2025-04-13 21:47:37 +0200

    Update ui

--- examples/ui.cpp
+++ examples/ui.cpp
@@ -1,8 +1,8 @@
 #include "pali.hpp"
-#include <memory>
-#include <unistd.h>
-#include <string>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <unistd.h>
 
 #define HEIGHT 43
 #define WIDTH 131
@@ -20,18 +20,26 @@ int main(int argc, char **argv) {
   Engine engine(HEIGHT, WIDTH, verbose, 141.0);
   std::string value = " ";
   PixelProperties pp = PixelProperties(value, RGB(0, 0, 0), RGB(0, 255, 0));
-  auto so1 = std::make_unique<StringObject>(StringObject(Point(10, 10), "Hello world", RGB(0, 0, 0), RGB(255, 255, 255)));
+  auto so1 = std::make_unique<StringObject>(StringObject(
+      Point(10, 10), "Hello world", RGB(0, 0, 0), RGB(255, 255, 255)));
   engine.addObject(std::move(so1));
-  auto so2 = std::make_unique<RectangleObject>(RectangleObject(Point(9, 9), pp, 3, 13, false));
+  auto so2 = std::make_unique<RectangleObject>(
+      RectangleObject(Point(9, 9), pp, 3, 13, false));
   engine.addObject(std::move(so2));
-  auto so3 = std::make_unique<InputFieldObject>(InputFieldObject(Point(43, 43), "Hello world2 example of a input fieldHello world2 example of a input fieldHello world2 example of a input fieldHello world2 example of a input fieldHello world2 example of a input fieldHello world2 example of a input field", 24, 24, RGB(242, 242, 242), RGB(84, 84, 84), false));
+  auto so3 = std::make_unique<InputFieldObject>(InputFieldObject(
+      Point(43, 43),
+      "Hello world2 example of a input fieldHello world2 example of a input "
+      "fieldHello world2 example of a input fieldHello world2 example of a "
+      "input fieldHello world2 example of a input fieldHello world2 example of "
+      "a input field",
+      24, 24, RGB(242, 242, 242), RGB(84, 84, 84), false));
   engine.addObject(std::move(so3));
   engine.setPosition(Point(24, 24));
   int i = 0;
   while (1) {
     engine.loop();
     usleep(434);
-    //engine.emptyObjs();
+    // engine.emptyObjs();
     i += 1;
   }
   return 0;
