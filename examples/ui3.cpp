4e05e690637a65a6241f96fd4eb010f8acbbd0f6
m-heim <maximilian.heim@protonmail.com>
2025-04-14 23:13:10 +0200

    Update interface

--- examples/ui3.cpp
+++ examples/ui3.cpp
@@ -1,6 +1,6 @@
-#include <pali.hpp>
 #include <iostream>
 #include <memory>
+#include <pali.hpp>
 #include <string>
 #include <unistd.h>
 
@@ -26,19 +26,22 @@ int main(int argc, char **argv) {
   auto so2 = std::make_unique<RectangleObject>(
       RectangleObject(Point(9, 9), pp, 3, 13, false));
   engine.addObject(std::move(so2));
-  auto so3 = std::make_unique<InputFieldObject>(InputFieldObject(
-      Point(43, 43),
-      "Hello world",
-      24, 24, RGB(242, 242, 242), RGB(84, 84, 84), false));
+  auto so3 = std::make_unique<InputFieldObject>(
+      InputFieldObject(Point(43, 43), "Hello world", 24, 24, RGB(242, 242, 242),
+                       RGB(84, 84, 84), false));
   engine.addObject(std::move(so3));
-  auto so4 = std::make_unique<FrameObject>(FrameObject(Point(10, 10), 10, 10, RGB(131, 131, 131), true));
-  auto so5 = std::make_unique<StringObject>(StringObject(Point(4, 4), "Hello world", RGB(255, 255, 255), RGB(0, 0, 0)));
-  auto so6 = std::make_unique<SnowObject>(SnowObject(Point(1, 1), PixelProperties("o", RGB(255, 255, 255), RGB(0, 0, 0)), 3, 0, 1));
+  auto so4 = std::make_unique<FrameObject>(
+      FrameObject(Point(10, 10), 10, 10, RGB(131, 131, 131), true));
+  auto so5 = std::make_unique<StringObject>(StringObject(
+      Point(4, 4), "Hello world", RGB(255, 255, 255), RGB(0, 0, 0)));
+  auto so6 = std::make_unique<SnowObject>(SnowObject(
+      Point(1, 1), PixelProperties("o", RGB(255, 255, 255), RGB(0, 0, 0)), 3, 0,
+      1));
   auto so7 = std::make_unique<SphereObject>(SphereObject(Point(7, 7), pp, 3));
   so4->addObject(std::move(so5));
   so4->addObject(std::move(so6));
   so4->addObject(std::move(so7));
-  
+
   so4->setVelocity(Point(1, 0));
   engine.addObject(std::move(so4));
   engine.setPosition(Point(24, 24));
